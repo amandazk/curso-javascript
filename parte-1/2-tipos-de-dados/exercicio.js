7b153ba7c4b716e4ad7e5d9dc0b716d38760feeb
amandazk <zacharukaz@gmail.com>
2021-01-14 17:38:21 -0300

    exercicios

---
+++ parte-1/2-tipos-de-dados/exercicio.js
@@ -0,0 +1,23 @@
+// Dica: frases(strings) devem ser colocadas entre aspas
+// (simples ou duplas) e números não leval aspas
+
+// Declare uma variável contendo uma string
+var um = 'carro';
+
+// Declare uma variável contendo um número dentro de uma string
+var dois = '2';
+
+// Declare uma variável com sua idade
+var idade = 23;
+
+// Declare duas variáveis, uma com seu nome e outra com 
+// seu sobrenome e some as mesmas
+var nome = 'Amanda';
+var sobrenome = 'Zacharuk';
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
